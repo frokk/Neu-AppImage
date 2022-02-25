3264ca3df7a7f2637ffca13fd58b635dea81974a
ADITYA MISHRA <75035219+DEVLOPRR@users.noreply.github.com>
2022-02-25 17:37:03 +0530

    Converted JavaScript API To TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,63 @@
-const fs = require('fs');
-const path = require('path');
-const { execSync } = require('child_process');
-const currentOSArch = require('process').arch;
-const platform = require('process').platform;
+import fs from "fs"
+import path from "path";
+import { execSync } from "child_process";
+import { platform } from "process"
 
-/**
- * Categories Defination
- * @typedef {string[]} Categories
- */
+const currentOSArch = require("os").arch;
 
-/**
- * AppImage Arguments
- * @typedef {object} Options
- * @property {string} programName - Program Name.
- * @property {string} genericName - Generic Program Name.
- * @property {string} description - Program Description.
- * @property {string} icon - Path to the icon of the program (512x512).
- * @property {Categories} categories - Program category. (Available Categories - Audio, Video, AudioVideo, Development, Education, Game, Graphics, Network, Office, Science, Settings, System, Utility)
- * @property {string} workingDir - The working directory to run the program in.
- * @property {string} arch - Program Architecture (x64 or x32)
- * @property {boolean} showAppImgToolOutput - if true, it will show the AppImage Tool output
- */
+type AppImageOptions = {
+	programName: string,
+	genericName: string,
+	description: string,
+	version: string,
+	icon: string,
+	categories: "Audio" | "Video" | "AudioVideo" | "Development" | "Education" | "Game" | "Graphics" | "Network" | "Office" | "Science" | "Settings" | "System" | "Utility" | [ "Audio" | "Video" | "AudioVideo" | "Development" | "Education" | "Game" | "Graphics" | "Network" | "Office" | "Science" | "Settings" | "System" | "Utility" ],
+	workingDir: string,
+	arch: "x86_64" | "i386",
+	showAppImgToolOutput: boolean
+}
 
-/**
- * @class AppImage
- */
-class AppImage {
-	/**
-	 * @param {string} executable Path To The Main Executable
-	 * @param {string} resource Path To The Resource.neu File
-	 * @param {string[]} files Additional Files You Want To Include in AppImage
-	 * @param {string} outdir Path To Output The AppImage
-	 * @param {Options} options AppImage Options
-	 */
-	constructor(executable, resource, files, outdir, options) {
+export const STATUS_CODE = {
+	SUCCESS: 0,
+	PATH_NOT_FOUND: 1,
+	UNSUPPORTED_OS_ARCHITECTURE: 2,
+	UNSUPPORTED_ARCHITECTURE: 3,
+	UNSUPPORTED_OS: 4,
+	UNKNOWN: 5
+}
+
+export const APPIMAGE_CATEGORIES = {
+	AUDIO: "Audio",
+	VIDEO: "Video",
+	AUDIOVIDEO: "AudioVideo",
+	DEVELOPMENT: "Development",
+	EDUCATION: "Education",
+	GAME: "Game",
+	GRAPHICS: "Graphics",
+	NETWORK: "Network",
+	OFFICE: "Office",
+	SCIENCE: "Science",
+	SETTINGS: "Settings",
+	SYSTEM: "System",
+	UTILITY: "Utility"
+}
+
+export class AppImage {
+	public executable: string;
+	public resource: string;
+	public files: string[];
+	public outdir: string;
+	public options: AppImageOptions;
+
+	private _tempDir: string | null = null;
+
+	constructor(
+		executable: string,
+		resource: string,
+		files: string[],
+		outdir: string,
+		options: AppImageOptions
+	) {
 		this.executable = executable;
 		this.resource = resource;
 		this.files = files;
@@ -41,52 +65,17 @@ class AppImage {
 		this.options = options;
 	}
 
-	_tempDir = null;
-
-	static get code() {
-		return {
-			SUCCESS: 0,
-			PATH_NOT_FOUND: 1,
-			UNSUPPORTED_OS_ARCHITECTURE: 2,
-			UNSUPPORTED_ARCHITECTURE: 3,
-			UNSUPPORTED_OS: 4,
-			UNKNOWN: 5
-		}
-	}
-
-	static get Category() {
-		return {
-			AUDIO: "Audio",
-			VIDEO: "Video",
-			AUDIOVIDEO: "AudioVideo",
-			DEVELOPMENT: "Development",
-			EDUCATION: "Education",
-			GAME: "Game",
-			GRAPHICS: "Graphics",
-			NETWORK: "Network",
-			OFFICE: "Office",
-			SCIENCE: "Science",
-			SETTINGS: "Settings",
-			SYSTEM: "System",
-			UTILITY: "Utility"
-		}
-	}
-
-	/**
-	 * Build The AppImage
-	 * @returns {object} Object Containing Exit Code, Description
-	 */
-	build() {
+	build(): { code: number, text: string } {
 		if (platform != "linux") {
 			return {
-				code: AppImage.code.UNSUPPORTED_OS,
+				code: STATUS_CODE.UNSUPPORTED_OS,
 				text: `AppImage Can Be Only Built on Linux, if you're on Windows Use WSL`
 			}
 		}
 		/**
 		 * Function To Clear Temporary Directory
 		 */
-		 function removeTemp(dir) {
+		 function removeTemp(dir: string) {
 			fs.rmSync(dir, {
 				recursive: true,
 				force: true
@@ -97,29 +86,29 @@ class AppImage {
 		 * make a file executable.
 		 * @param {String} file File Path To Make Executable.
 		 */
-		function makeFileExecutable(file) {
+		function makeFileExecutable(file: string) {
 			try {
 				let fd = fs.openSync(file, "r");
 				fs.fchmodSync(fd, 0o777);
-			} catch (error) {
-				throw new Error(error);
+			} catch (err: any) {
+				throw new Error(err);
 			}
 		}
 
 		try {
 			if (!fs.existsSync(this.executable)) {
 				return {
-					code: AppImage.code.PATH_NOT_FOUND,
+					code: STATUS_CODE.PATH_NOT_FOUND,
 					text: `File '${this.executable}' Doesn't Exist`
 				}
 			} else if (!fs.existsSync(this.resource)) {
 				return {
-					code: AppImage.code.PATH_NOT_FOUND,
+					code: STATUS_CODE.PATH_NOT_FOUND,
 					text: `File '${this.executable}' Doesn't Exist`
 				}
 			} else if (!fs.existsSync(this.outdir)) {
 				return {
-					code: AppImage.code.PATH_NOT_FOUND,
+					code: STATUS_CODE.PATH_NOT_FOUND,
 					text: `Folder '${this.outdir}' Doesn't Exist`,
 				}
 			}
@@ -129,7 +118,7 @@ class AppImage {
 					this.files[i] = path.resolve(this.files[i])
 					if (!fs.existsSync(this.files[i])) {
 						return {
-							code: AppImage.code.PATH_NOT_FOUND,
+							code: STATUS_CODE.PATH_NOT_FOUND,
 							text: `File '${this.files[i]}' Doesn't Exist`
 						}
 					}
@@ -142,27 +131,29 @@ class AppImage {
 			let apprunFile = path.join(appDir, "AppRun");
 			let desktopFile = path.join(appDir, `${this.options.programName}.desktop`);
 			let appimageToolPath;
-		
-			if (currentOSArch == 'x64') {
+
+			if (currentOSArch() == 'x64') {
 				appimageToolPath = path.resolve(__dirname, '../appimagetool/appimagetool-x86_64.AppImage');
-			} else if (currentOSArch == 'x32' || currentOSArch == 'ia32') {
+			} else if (currentOSArch() == 'x32' || currentOSArch() == 'ia32') {
 				appimageToolPath = path.resolve(__dirname, '../appimagetool/appimagetool-i686.AppImage');
 			} else {
-				removeTemp(this._tempDir);
+				if (this._tempDir) removeTemp(this._tempDir);
 				return {
-					code: AppImage.code.UNSUPPORTED_OS_ARCHITECTURE,
-					text: `Current OS Architecture is Unsupported - ${currentOSArch}`
+					code: STATUS_CODE.UNSUPPORTED_OS_ARCHITECTURE,
+					text: `Current OS Architecture is Unsupported - ${currentOSArch()}`
 				}
 			}
-		
+
 			fs.mkdirSync(appDir);
 			fs.mkdirSync(binDir, {
 				recursive: true
 			});
-		
+
 			let AppRunData = `#!/bin/sh\nHERE="$(dirname "$(readlink -f "\${0}")")"\nEXEC="\${HERE}/usr/bin/${path.basename(this.executable)}"\nexec "\${EXEC}"`
 			let DesktopEntry = "[Desktop Entry]\nType=Application\n";
 			DesktopEntry += `Name=${this.options.programName}\n`;
+			DesktopEntry += `X-AppImage-Name=${this.options.programName}\n`;
+			if (this.options.version) DesktopEntry += `X-AppImage-Version=${this.options.version}\n`;
 			DesktopEntry += `Exec=AppRun\n`;
 			if (this.options.genericName) DesktopEntry += `GenericName=${this.options.genericName}\n`;
 			if (this.options.description) DesktopEntry += `Comment=${this.options.description}\n`;
@@ -172,22 +163,22 @@ class AppImage {
 				DesktopEntry += `${this.options.categories[i]};`
 			}
 			DesktopEntry += `\n`
-		
+
 			if (this.options.icon) {
 				let iconFile = path.join(appDir, `applicationIcon.png`);
 				fs.copyFileSync(this.options.icon, iconFile);
 				DesktopEntry += `Icon=${path.basename(iconFile, '.png')}\n`
 			}
 			if (this.options.workingDir) DesktopEntry += `Path=${this.options.workingDir}`
-		
+
 			fs.writeFileSync(apprunFile, AppRunData, {
 				encoding: 'utf-8'
 			});
-		
+
 			fs.writeFileSync(desktopFile, DesktopEntry, {
 				encoding: 'utf-8'
 			});
-		
+
 			makeFileExecutable(apprunFile);
 			makeFileExecutable(desktopFile);
 
@@ -201,19 +192,19 @@ class AppImage {
 			}
 
 			makeFileExecutable(this.executable);
-		
+
 			let command = ``;
 			let outputFile = ``;
-			if (this.options.arch == 'x64') {
+			if (this.options.arch == 'x86_64') {
 				command = `ARCH=x86_64 ${appimageToolPath} "${appDir}"`
 				outputFile = path.resolve("./", `${this.options.programName.replace(/ /g, '_')}-x86_64.AppImage`)
-			} else if (this.options.arch == `x32`) {
+			} else if (this.options.arch == `i386`) {
 				command = `ARCH=i386 ${appimageToolPath} "${appDir}"`
 				outputFile = path.resolve("./", `${this.options.programName.replace(/ /g, '_')}-i386.AppImage`)
 			} else {
 				removeTemp(this._tempDir);
 				return {
-					code: AppImage.code.UNSUPPORTED_ARCHITECTURE,
+					code: STATUS_CODE.UNSUPPORTED_ARCHITECTURE,
 					text: `Unsupported AppImage Architecture Specified - ${this.options.arch}`
 				}
 			}
@@ -227,22 +218,22 @@ class AppImage {
 			let newFilePath = path.resolve(outputFile, path.join(path.resolve(this.outdir), path.basename(outputFile)));
 			fs.renameSync(outputFile, newFilePath);
 			return {
-				code: AppImage.code.SUCCESS,
+				code: STATUS_CODE.SUCCESS,
 				text: newFilePath
 			};
-		} catch (error) {
+		} catch (err: any) {
 			try {
 				if (this._tempDir) {
 					removeTemp(this._tempDir);
 					this._tempDir = null;
 				}
-			} catch (error) {}
+			} catch (err: any) {}
 			return {
-				code: AppImage.code.UNKNOWN,
-				text: error
+				code: STATUS_CODE.UNKNOWN,
+				text: err
 			}
 		}
 	}
 }
 
-module.exports = AppImage;
+// let app = new AppImage()
